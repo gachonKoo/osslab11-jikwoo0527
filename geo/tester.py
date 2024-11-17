66e59f63166efedf7ffabcd22af3cf5d3115a76c
jikwoo0527 <162891330+jikwoo0527@users.noreply.github.com>
2024-11-17 16:02:18 +0900

    [chore] change print order

--- geo/tester.py
+++ geo/tester.py
@@ -2,10 +2,11 @@ import geo.utils as utils
 
 # calculate the length of hypotenuse(c) when a=3 and b=4
 a, b = 3, 4
-c = 5.0
-print('c =', c)
+c = utils.pythagoras(a, b)
 
 # calculate the area of circle with radius r = 10
 r = 10
-area = 314.1592653589793
+area = utils.circle(r)
+
+print('c =', c)
 print('area =', area)
